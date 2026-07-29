a2652d714ac3bfff1a345af283761146f14de9e0
João Pedro Melo da Silva <jpmelodasilva309@gmail.com>
2026-07-29 15:06:30 -0300

    adding protection to main branch merge

--- src/test/java/com/jp/flowpay/API/integration/ApiApplicationTests.java
+++ src/test/java/com/jp/flowpay/API/integration/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jp.flowpay.API;
+package com.jp.flowpay.API.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/jp/flowpay/API/unit/GithubTest.java
+++ src/test/java/com/jp/flowpay/API/unit/GithubTest.java
@@ -1,4 +1,4 @@
-package com.jp.flowpay.API;
+package com.jp.flowpay.API.unit;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
