4ff706a2a2d24ebe0dc161a39b459a7e29aaa738
João Pedro Melo da Silva <jpmelodasilva309@gmail.com>
2026-07-30 20:34:44 -0300

    addind the 3 principal entitys

---
+++ src/main/java/com/jp/flowpay/API/entity/Ticket.java
@@ -0,0 +1,106 @@
+package com.jp.flowpay.API.entity;
+
+import com.jp.flowpay.API.enums.TicketStatus;
+
+import java.time.LocalDateTime;
+
+public class Ticket {
+    private Long id;
+
+    private String conversationRef;
+
+    private String subject;
+
+    private TicketStatus status;
+
+    private Long teamId;
+
+    private Long agentId;
+
+    private LocalDateTime createdAt;
+
+
+    public Ticket() {
+    }
+
+    public Ticket(Long id,
+                  String conversationRef,
+                  String subject,
+                  TicketStatus status,
+                  Long teamId,
+                  Long agentId,
+                  LocalDateTime createdAt) {
+
+        this.id = id;
+        this.conversationRef = conversationRef;
+        this.subject = subject;
+        this.status = status;
+        this.teamId = teamId;
+        this.agentId = agentId;
+        this.createdAt = createdAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getConversationRef() {
+        return conversationRef;
+    }
+
+    public void setConversationRef(String conversationRef) {
+        this.conversationRef = conversationRef;
+    }
+
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+
+    public TicketStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TicketStatus status) {
+        this.status = status;
+    }
+
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+
+    public Long getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(Long agentId) {
+        this.agentId = agentId;
+    }
+
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
