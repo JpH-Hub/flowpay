b9e2e03fae726b309ba7c4863217c27fad6c8c2b
João Pedro Melo da Silva <jpmelodasilva309@gmail.com>
2026-07-30 14:17:55 -0300

    inserting initial data seeding

---
+++ src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -0,0 +1,19 @@
+INSERT INTO teams (name)
+VALUES ('Cartões'),
+       ('Empréstimos'),
+       ('Outros Assuntos');
+
+
+INSERT INTO agents (name, team_id)
+VALUES
+    ('Cartões', 1),
+    ('Cartões', 1),
+    ('Cartões', 1),
+
+    ('Empréstimos', 2),
+    ('Empréstimos', 2),
+    ('Empréstimos', 2),
+
+    ('Outros Assuntos', 3),
+    ('Outros Assuntos', 3),
+    ('Outros Assuntos', 3);
