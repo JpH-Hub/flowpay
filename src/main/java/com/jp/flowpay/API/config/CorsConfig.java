9c7b0a8996a088b94c6e36159cd06066e0927776
João Pedro Melo da Silva <jpmelodasilva309@gmail.com>
2026-08-13 13:54:15 -0300

    feat(config): add global CORS configuration for frontend origin

---
+++ src/main/java/com/jp/flowpay/API/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.jp.flowpay.API.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Libera o Vite
+                        .allowedMethods("GET", "POST", "PATCH")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+
+}
